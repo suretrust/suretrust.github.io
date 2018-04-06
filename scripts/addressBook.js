4f46f2da9bf6e3e6cd10ef8eefd15c633e310e7a
Saheed Oladele <sdrfngg640@gmail.com>
2018-04-06 15:55:30 +0100

    updates

--- scripts/addressBook.js
+++ scripts/addressBook.js
@@ -11,7 +11,7 @@ function showForm() {
         form.style.display = "none";
         homeDtl.style.display = "block";
         homeDtl.style.display = "none";
-    };
+    }
 };
 
 function hideForm() {
@@ -27,40 +27,32 @@ function hideForm() {
         form.style.display = "block";
         homeDtl.style.display = "none";
         view.style.display = "none";
-    };
+    }
 };
 
-var form = document.getElementById("addForm");
 var addressBook = [];
-var view = document.getElementById("view");
-var homeDtl = document.getElementById("homeDtl");
-    
+
+function Contact(name,number,email) {
+    this.name = name;
+    this.number = number;
+    this.email = email;
+};
 
 function addContact(){
-    
+    event.preventDefault();
+
     var name = document.getElementById("name").value;
     var number = document.getElementById("number").value;
     var email = document.getElementById("email").value;
-
-    
-
-    if (name !== "" && number !== ""){
-
-        function Contact(name,number,email) {
-            this.name = name;
-            this.number = number;
-            this.email = email;
-        };
-
-        var newContact = new Contact(name,number,email);
-
-        addressBook.push(newContact);
-
-        console.log(addressBook);
         
-        clearForm();
-        showAddressBook()
-    };
+    var newContact = new Contact(name,number,email);
+
+    addressBook.push(newContact);
+    console.log(addressBook);
+
+    clearForm();
+    showAddressBook();
+
 };
 
 function clearForm() {
@@ -68,7 +60,7 @@ function clearForm() {
 
     for (var i = 0; i < formFields.length; i++) {
          formFields[i].value = "";
-    };
+    }
 };
 
 
@@ -83,11 +75,11 @@ function showAddressBook(){
         
     view.innerHTML = '';
     var str = '<div class="view">';
-    str += '<h1 class="text-uppercase h1"><strong>Contacts</strong></h1><br />'
+    str += '<h1 class="text-uppercase h1"><strong>Contacts</strong></h1><br />';
     for(var n = 0; n < addressBook.length; n++){
-        str += '<div class="btn-group-xs h11 text-capitalize"><a class="">' + addressBook[n].name + '</a>';
-            str += '<button class="btn btn-primary margin-left"' + n + '">Delete</button><button class="btn btn-primary margin-left"' + n + '">Edit</button></div>';
-    };
+        str += '<div class="btn-group-xs h11 text-capitalize" ><a id="nameID" data-id="' + n + '" onclick="viewContact()">' + addressBook[n].name + '</a>';
+        str += '<button class="btn btn-primary margin-left deleteBtn" id="deleteBtn" onclick="deleteContact()" data-id="' + n + '">Delete</button><button class="btn btn-primary margin-left" id="editBtn" onclick="editContact()" data-id="' + n + '">Edit</button></div>';
+    }
         
     str += '</div>';
 
@@ -97,14 +89,58 @@ function showAddressBook(){
 function viewContact() {
     var view = document.getElementById("view");
 
+    var nameID = document.getElementById("nameID");
+
+
+    var datID = nameID.dataset.id;
+    
+    console.log(datID);
+
     view.innerHTML = '';
 
     var show = '<div class="view">';
 
-    show += '<h1 class="text-uppercase h1"><strong>Contact details</strong></h1><br />'
-    show += `${nameHid}`
+    show += '<h1 class="text-uppercase h1"><strong>Contact details</strong></h1><br />';
+    show += `<p class="h11"> Name: ${addressBook[datID].name}<br/> Phone Number: ${addressBook[datID].number}<br/> Email: ${addressBook[datID].email}</p>`;
+    show += '<button class="btn btn-primary margin-left" id="backBtn" onclick="showAddressBook()">Back to Contacts</button>';
     show += '</div>';
     
-
     view.innerHTML += show;
-}
+};
+
+function deleteContact() {
+    var deleteBtn = document.getElementById("deleteBtn");
+    var dataID = deleteBtn.dataset.id;
+
+    console.log(dataID);
+
+    alert("Warning: Deleted contacts are lost forever");
+    addressBook.splice(dataID, 1);
+    showAddressBook();
+};
+
+function editContact() {
+    var editBtn = document.getElementById("editBtn");
+    var dataID = editBtn.getAttribute("data-id");
+
+    var name = document.getElementById("name").value;
+    var number = document.getElementById("number").value;
+    var email = document.getElementById("email").value;
+
+    var editForm = document.getElementById("editForm");
+    var form = document.getElementById("addForm");
+    var homeDtl = document.getElementById("homeDtl");
+    var view = document.getElementById("view");
+
+    if (editForm.style.display === "none") {
+        editForm.style.display = "block";
+        form.style.display = "none";
+        homeDtl.style.display = "none";
+        view.style.display = "none";
+    } else {
+        form.style.display = "none";
+        view.style.display = "block";
+        homeDtl.style.display = "none";
+        editForm.style.display = "none";
+    }
+};
